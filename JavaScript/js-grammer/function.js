6dda2b0b0b55d0a1e0dbfda9097e6d1ec440e65a
ho2ri2s <ho2ri2s.github@gmail.com>
2019-12-28 00:01:07 +0900

    js grammer

---
+++ JavaScript/js-grammer/function.js
@@ -0,0 +1,7 @@
+var myBirthday = new Date(1982, 11, 17, 12, 30);
+function updateParagraph(){
+    var now = new Date();
+    var seconds = (now.getTime() - myBirthday.getTime()) / 1000;
+    document.getElementById('birth-time').innerText = '生まれてから' + seconds + '秒経過';
+}
+setInterval(updateParagraph, 50);
---
+++ JavaScript/js-grammer/logic.js
@@ -0,0 +1,18 @@
+var age = 16;
+var result = null;
+
+if (age > 20) {
+    result = "成年";
+} else {
+    result = "未成年";
+}
+
+document.write(result)
+
+var a = [];
+console.log(a);
+a.push('X');
+console.log(a);
+a.push('Y');
+console.log(a);
+console.log(a.length);

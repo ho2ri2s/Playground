04b44261394bab540196e8f6071d5fd5e0670692
Yuta Hori <ho2ri2s.github@gmail.com>
2020-01-04 01:05:46 +0900

    practice HTTP method

--- JavaScript/node-js-http/index.js
+++ JavaScript/node-js-http/index.js
@@ -1,11 +1,32 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
+    const now = new Date();
+    console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
     res.writeHead(200, {
         'Content-Type': 'text/plain; charset=utf-8'
     });
-    res.write(req.headers['user-agent']);
+    // requestで使われたメソッドで判定
+    switch (req.method) {
+        case 'GET':
+            res.write('GET ' + req.url);
+            break;
+        case 'POST':
+            //curlで -dオプションをつけるとPOSTでリクエストが飛ぶ
+            res.write('POST ' + req.url);
+            let rawData = '';
+            req.on('data', (chunk) => {
+                rawData = rawData + chunk;
+            }).on('end', () => {
+                console.info('[' + now + '] Data posted: ' + rawData);
+            });
+            break;
+        case 'DELETE':
+            res.write('DELETE' + req.url);
+            break;
+        default:
+            break;
+    }
     res.end();
 }).on('error', (e) => {
     console.error('[' + new Date() + '] Server Error', e);

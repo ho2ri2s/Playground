934b29f1f8ecbf800e0195730d7e9be49e2a5448
ho2ri2s <ho2ri2s.github@gmail.com>
2020-02-18 01:00:18 +0900

    validate test

--- Android/UIAnimation/app/src/main/java/com/example/uianimation/ui/UiActivity.kt
+++ Android/UIAnimation/app/src/main/java/com/example/uianimation/ui/UiActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.uianimation.R
 import com.example.uianimation.databinding.ActivityUiBinding
@@ -22,9 +23,20 @@ class UiActivity : AppCompatActivity() {
 
         binding.btnTimePicker.setOnClickListener {
             val calendar = Calendar.getInstance()
-            TimePickerDialog(this, { _, hour, minute ->
-                Log.d("MYTAG", "hour: $hour, minute: $minute")
-            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show()
+            val dialog = TimePickerDialog(
+                this, { _, hour, minute ->
+                    Log.d("MYTAG", "hour: $hour, minute: $minute")
+                    val cal = Calendar.getInstance(Locale.US).apply {
+                        set(Calendar.HOUR_OF_DAY, hour)
+                        set(Calendar.MINUTE, minute)
+                    }
+                    val isFuture = (cal.timeInMillis - Date().time) > 0
+                    if (isFuture) {
+                        Toast.makeText(this@UiActivity, "Future~!", Toast.LENGTH_LONG).show()
+                    }
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false
+            )
+            dialog.show()
         }
     }
 }
